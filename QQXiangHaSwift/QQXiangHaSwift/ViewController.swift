c92dd93dea2ffb266c93d4936ae273b02a32c2b8
xuchaoqi <2543209118@qq.com>
2016-12-30 14:03:19 +0800

    newProj

---
+++ QQXiangHaSwift/QQXiangHaSwift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  QQXiangHaSwift
+//
+//  Created by xuchaoqi on 2016/12/30.
+//  Copyright © 2016年 MS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
